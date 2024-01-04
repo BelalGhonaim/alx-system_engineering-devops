8764daf0a4d8fe395dc47f9a943a82784f5cb30f
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-04 21:44:14 +0200

    fixed file 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Script to export data in the CSV format"""
-import csv
 import requests
 import sys
+import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
@@ -10,14 +10,16 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    json_info = {
+        user_id: [
+            {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+            }
+            for todo in todos
+        ]
+    }
+
     with open("{}.json".format(user_id), "w") as jsonfile:
-        jsonfile.write("[")
-        for todo in todos:
-            jsonfile.write("{")
-            jsonfile.write('"task": "{}", '.format(todo.get("title")))
-            jsonfile.write('"completed": {}, '.format(todo.get("completed")))
-            jsonfile.write('"username": "{}"'.format(username))
-            jsonfile.write("}")
-            if todo != todos[-1]:
-                jsonfile.write(", ")
-        jsonfile.write("]")
+        json.dump(json_info, jsonfile, indent=2)
