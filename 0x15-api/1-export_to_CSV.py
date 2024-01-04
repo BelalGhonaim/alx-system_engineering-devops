c0730d309ce3c8f3a811aa924b7dac3093629796
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-04 20:21:40 +0200

    fixed file 1-export csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" script to export data in the CSV format"""
-if __name__ == "__main__":
-    import csv
-    import requests
-    import sys
+""" Script to export data in the CSV format"""
+import csv
+import requests
+import sys
 
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    with open("{}.csv".format(sys.argv[1]), "w") as csvfile:
+    with open("{}.csv".format(sys.argv[1]), "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
         for task in todos:
+            completed_status = "completed" if task.get("completed") else "not completed"
             writer.writerow([sys.argv[1], user.get("username"),
-                             task.get("completed"), task.get("title")])
+                             completed_status, task.get("title")])
