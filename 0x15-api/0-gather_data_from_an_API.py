2ca86d4235f86f8326959159d8ebcd13f0315f99
belalghonaim99 <belalpop1999@yahoo.com>
2024-01-04 06:22:50 -0800

    updated file 0 pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos", params={"userId": user_id}).json()
-    
+
     completed = []  # Define the 'completed' list
-    
+
     for todos in todo:
         if todos.get("done") is True:
             completed.append(todos.get("title"))
-    
+
     print("Employee {} is done with tasks({}/{}):"
-          .format(user.get("name"), len(completed), len(todo)))  # Fix the generator expression
-    
+          .format(user.get("name"), len(completed), len(todo)))
+
     for task_title in completed:
         print("\t {}".format(task_title))
