531533bf2b27ef3bb9adea6a4db2205a29fb4fbf
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-16 23:59:05 +0200

    updated file 1-top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,26 +9,18 @@ import requests
 def top_ten(subreddit):
 
 
-    url_sub = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                        .format(subreddit),
-                        headers={"User-Agent": "My-User-Agent"},
-                        allow_redirects=False)
-    if url_sub.status_code >= 300:
-        print('None')
-    else:
-        [print(titles.get("data").get("title"))
-            for titles in url_sub.json().get("data").get("children")]
     sub_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    
     sub_head = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    title_limits = {
+    title_limites = {
         "limit": 10
     }
-    sub_res = requests.get(sub_url, headers=sub_head, params=title_limits,
+    sub_rep = requests.get(sub_url, headers=sub_head, params=title_limites,
                             allow_redirects=False)
-    if sub_res.status_code == 404:
+    if sub_rep.status_code == 404:
         print("None")
         return
-    output = sub_res.json().get("data")
-    [print(y.get("data").get("title")) for y in output.get("children")]
+    response = sub_rep.json().get("data")
+    [print(y.get("data").get("title")) for y in response.get("children")]
