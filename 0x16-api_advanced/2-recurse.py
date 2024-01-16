b7a484b9352f328862c986542e4f03014c794ce7
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-17 00:52:45 +0200

    updated file 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,24 +7,26 @@ import requests
 
 def recurse(subreddit, hot_list=[], count=0, after=None):
 
-    sub_url = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"count": count, "after": after},
-                            headers={"User-Agent": "My-User-Agent"},
+    sub_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    sub_head = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    para = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    sub_resp = requests.get(sub_url, headers=sub_head, params=para,
                             allow_redirects=False)
-    if sub_url.status_code >= 404:
+    if sub_resp.status_code == 404:
         return None
 
-    count_hot_list = hot_list + [y.get("data").get("title")
-                        for y in sub_url.json()
-                        .get("data")
-                        .get("children")]
+    response = sub_resp.json().get("data")
+    after = response.get("after")
+    count += response.get("dist")
+    for c in response.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-
-    sub_resp = sub_url.json()
-    if not sub_resp.get("data").get("after"):
-        return count_hot_list
-
-
-    return recurse(subreddit, count_hot_list, sub_resp.get("data").get("count"),
-                   sub_resp.get("data").get("after"))
+    if after > 0:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
