3f6ec0ad938df82b7fecda1c32c01a281147f6b8
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-16 19:55:59 +0200

    add README file and 0-subs file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers
+"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    num_of_sub = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if num_of_sub.status_code >= 300:
+        return 0
+
+    return num_of_sub.json().get("data").get("subscribers")
