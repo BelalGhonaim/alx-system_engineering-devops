a1bb764695d21bb654b6412e9682a3bcd21dff7d
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-16 20:25:38 +0200

    oh forget to import requests moduel :D

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and returns the number of subscribers
 """
-
+import requests
 
 def number_of_subscribers(subreddit):
     url_sub = "https://www.reddit.com/r/{}/about.json".format(subreddit)
