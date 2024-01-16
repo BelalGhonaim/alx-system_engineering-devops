ab3b729cfb6a957a089626a902c1e572fba80c41
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-16 20:16:17 +0200

    misssing comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,6 @@
 
 
 def number_of_subscribers(subreddit):
-    """Return  total number of subscribers on  given subreddit."""
     url_sub = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     sub_headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
