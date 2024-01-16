0bbab55b583773b5b1311c66ad8f9c9a379c7fd1
belalghonaim99 <belalpop1999@yahoo.com>
2024-01-16 11:38:21 -0800

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,9 +9,9 @@ import requests
 def top_ten(subreddit):
 
     url_sub = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
     if url_sub.status_code >= 300:
         print('None')
     else:
