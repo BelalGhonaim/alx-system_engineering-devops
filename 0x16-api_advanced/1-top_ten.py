47a6c68235a982e260228f23dbf9f5fadbaeabf8
belalghonaim99 <belalpop1999@yahoo.com>
2024-01-16 14:01:00 -0800

    updated file 1 and chmod files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,8 @@ import requests
 
 
 def top_ten(subreddit):
-
-
     sub_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    
+
     sub_head = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -18,7 +16,7 @@ def top_ten(subreddit):
         "limit": 10
     }
     sub_rep = requests.get(sub_url, headers=sub_head, params=title_limites,
-                            allow_redirects=False)
+                           allow_redirects=False)
     if sub_rep.status_code == 404:
         print("None")
         return
