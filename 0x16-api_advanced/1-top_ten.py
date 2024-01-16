f9730b40de47603328f305dd1e0ca859e03f3086
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-16 21:36:30 +0200

    update some functions

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,13 +7,17 @@ import requests
 
 
 def top_ten(subreddit):
-
-    url_sub = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+    sub_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    sub_head = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    title_limits = {
+        "limit": 10
+    }
+    sub_res = requests.get(sub_url, headers=sub_head, params=title_limits,
                             allow_redirects=False)
-    if url_sub.status_code >= 300:
-        print('None')
-    else:
-        [print(titles.get("data").get("title"))
-            for titles in url_sub.json().get("data").get("children")]
+    if sub_res.status_code == 404:
+        print("None")
+        return
+    output = sub_res.json().get("data")
+    [print(y.get("data").get("title")) for y in output.get("children")]
