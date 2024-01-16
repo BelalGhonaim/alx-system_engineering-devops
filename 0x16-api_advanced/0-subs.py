272e0ed7e68faaaa43d5e753fadc3cf866fc63cf
belalghonaim99 <belalpop1999@yahoo.com>
2024-01-16 10:03:01 -0800

    updated pycodsestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,11 @@ def number_of_subscribers(subreddit):
     import requests
 
     num_of_sub = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                              .format(subreddit),
+                              headers={"User-Agent": "My-User-Agent"},
+                              allow_redirects=False)
     if num_of_sub.status_code >= 300:
         return 0
 
-    return num_of_sub.json().get("data").get("subscribers")
+
+return num_of_sub.json().get("data").get("subscribers")
