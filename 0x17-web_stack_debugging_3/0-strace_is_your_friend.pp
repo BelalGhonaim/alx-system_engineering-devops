13768015c10f32dd432dabb34207e73bdb183a6b
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-19 01:33:55 +0200

    updated some isssues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # Using strace, find out why Apache is returning a 500 error.
-
 exec { 'fix_phpp':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
